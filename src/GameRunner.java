9f2219aa347999cd48a671b1e7c080f926ae210f
ChijiokeOkoro <67354170+ChijiokeOkoro@users.noreply.github.com>
2021-08-26 22:52:14 -0400

    Update GameRunner.java

--- src/GameRunner.java
+++ src/GameRunner.java
@@ -9,14 +9,25 @@ import java.util.Queue;
 import java.util.Random;
 
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 public class GameRunner extends JPanel implements KeyListener, ActionListener{
 	
 	private Queue<Pieces> pieceOrder = new LinkedList<>();
+	private Pieces piece;
+	private Timer timer;
+	private int delay = 1, count = 0;
 	
 	public GameRunner() {
 		for(int i = 0; i < 5; i++)
-			pieceOrder.add(new Pieces( new Random().nextInt(7) ) );
+			pieceOrder.add(new Pieces( 0/*new Random().nextInt(7)*/) );
+		
+		addKeyListener(this);
+		setFocusable(true);
+		this.setFocusTraversalKeysEnabled(false);
+		
+		timer = new Timer(delay, this);
+		timer.start();
 		
 	}
 	
@@ -28,22 +39,37 @@ public class GameRunner extends JPanel implements KeyListener, ActionListener{
 		
 		//tetris grid
 		g.setColor(Color.gray);
-		for( int i = 0; i < 20; i++) {
+		for( int i = 0; i < 21; i++) {
 			g.drawLine(25, 50 + i*25, 275, 50 + i*25);
 		}
 		
 		for( int i = 0; i < 11; i++) {
-			g.drawLine( 25 + i*25, 50 , 25 + i*25, 525);
+			g.drawLine( 25 + i*25, 50 , 25 + i*25, 550);
 		}
+		
+		// draws the piece on the board
+		if(piece != null)
+			piece.drawShape(g);
 		
 		
 		g.dispose();
 	}
+	
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		timer.start();
 		
+		count++;
 		
+		if(piece == null || piece.dropped) {
+			piece = pieceOrder.remove();
+			pieceOrder.add(new Pieces( new Random().nextInt(7) ) );
+		}
+		
+		if(count % 100 == 0) {
+			piece.pt.y += 25;
+		}
 		
 		repaint();
 	}
@@ -56,7 +82,60 @@ public class GameRunner extends JPanel implements KeyListener, ActionListener{
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
+		if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+			switch(piece.piece){
+				case 0: 
+					if(piece.pt.x > 25) piece.pt.x -= 25;
+					break;
+				case 1:
+					if(piece.rotation % 4 == 0 && piece.pt.x > 50) piece.pt.x -= 25;
+					else if((piece.rotation % 4 == 1 || piece.rotation % 4 == -3) && piece.pt.x > 25) piece.pt.x -= 25;
+					else if((piece.rotation % 4 == 2 || piece.rotation % 4 == -2) && piece.pt.x > 50) piece.pt.x -= 25;
+					else if((piece.rotation % 4 == 3 || piece.rotation % 4 == -1) && piece.pt.x > 50) piece.pt.x -= 25;
+					break;
+				case 2: 
+					break;
+				case 3: 
+					break;
+				case 4: 
+					break;
+				case 5: 
+					break;
+				default:
+					break;
+			}
+			
+		}
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			switch(piece.piece){
+				case 0: 
+					if(piece.pt.x < 225) piece.pt.x += 25;
+					break;
+				case 1:
+					if(piece.rotation % 4 == 0 && piece.pt.x < 225) piece.pt.x += 25;
+					else if((piece.rotation % 4 == 1 || piece.rotation % 4 == -3) && piece.pt.x < 225) piece.pt.x += 25;
+					else if((piece.rotation % 4 == 2 || piece.rotation % 4 == -2) && piece.pt.x < 225) piece.pt.x += 25;
+					else if((piece.rotation % 4 == 3 || piece.rotation % 4 == -1) && piece.pt.x < 250) piece.pt.x += 25;
+					break;
+				case 2: 
+				break;
+				case 3: 
+				break;
+				case 4: 
+				break;
+				case 5: 
+				break;
+				default:
+				break;
+			}
+		}
+		if(e.getKeyCode() == KeyEvent.VK_X) {
+			piece.rotateLeft();
+		}
+		if(e.getKeyCode() == KeyEvent.VK_Z) {
+			piece.rotateRight();			
+		}
+		
 		
 	}
 
